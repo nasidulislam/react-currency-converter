0eaaaf8a3ec592a0d44aef3e4e8b61404fd85fa3
Nasidul Islam <nasidulislam@gmail.com>
2019-03-17 21:39:59 -0400

    app-styling: add node-sass and variabilize stuff.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
-import '../styles/App.css';
+import '../styles/App.scss';
 
 class App extends Component {
 	render() {
 		return (
-			<div></div>
+			<div className="App">
+				<header className="App-header">
+					<h1>Currency Converter</h1>
+				</header>
+			</div>
 		);
 	}
 }
